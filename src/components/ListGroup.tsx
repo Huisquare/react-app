08b2f791704cb98f30eaebe6c0a6547430794b84
Huihui <lihuihui99@outlook.com>
2025-09-02 15:14:07 +0800

    1.5.2 ternary operator within return statement to replace the if-else statement

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,17 +4,10 @@ const ListGroup = () => {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Seoul"];
   items = [];
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No items found</p>
-      </>
-    );
-
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
