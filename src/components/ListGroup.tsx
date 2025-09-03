ed9fead88d32adec209c7d8757b6694d5689ab25
Huihui <lihuihui99@outlook.com>
2025-09-03 18:52:35 +0800

    1.8.2 refactor the function definition to a constant and made reference to it within jsx return statement

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,17 @@
-import React from "react";
+import { MouseEvent } from "react";
 
 const ListGroup = () => {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Seoul"];
 
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h1>List</h1>
-      {length === 0 && <p>No item found</p>}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log("clicked")}
-          >
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
