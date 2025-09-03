a5240b01f5a8c48e62c6d4f93af81df50c2d4047
Huihui <lihuihui99@outlook.com>
2025-09-03 17:20:36 +0800

    1.9 use hooks to set and update variables

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListGroup = () => {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Seoul"];
 
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List</h1>
-      {length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log("clicked")}
+            onClick={() => setSelectedIndex(index)}
           >
             {item}
           </li>
