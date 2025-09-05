e2a31d1ecf6ec95af2d070e35349d5e5eab096fb
Huihui <lihuihui99@outlook.com>
2025-09-05 21:08:12 +0800

    1.12.2 use ReactNode as a datatype for children in order to take in other html elements like <h1>, <span> as input

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Alert from "./components/Alert";
 function App() {
   return (
     <div>
-      <Alert> Hello </Alert>
+      <Alert>
+        <h1>Hello</h1>
+      </Alert>
     </div>
   );
 }
