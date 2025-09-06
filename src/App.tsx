50e15390c6acf962fd7b01667aa23d7f2e1ada50
Huihui <lihuihui99@outlook.com>
2025-09-06 21:27:12 +0800

    1.13.1 added a button component with customisable button text and function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
+  const buttonFunc = () => {
+    console.log("clicked");
+  };
   return (
     <div>
       <Alert>
         <h1>Hello</h1>
       </Alert>
+
+      <Button onClick={buttonFunc}>text here</Button>
     </div>
   );
 }
