3470e9250e788c6aa6c21796fd3459b622b8515c
Huihui <lihuihui99@outlook.com>
2025-09-23 14:40:45 +0800

    3.0 Creating a basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      {/* <ExpandableText>Hello World</ExpandableText> */}
-      <ExpandableText>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
-        accusamus molestias laborum ex minima pariatur minus dolore non sint
-        repudiandae accusantium voluptates exercitationem quia eveniet rem
-        quasi, delectus fuga? Sed quisquam quae in. Sit, delectus quae? Tenetur
-        debitis, culpa aliquid veniam, non, maiores quam numquam nostrum nulla a
-        deserunt eius temporibus corporis soluta itaque consectetur dolor quis
-        veritatis. Totam, mollitia explicabo, magnam ipsa quidem at error vitae
-        officiis non ea doloribus. Nulla nihil nostrum optio tenetur eaque neque
-        dolore temporibus commodi quam? Neque minima natus maiores deleniti
-        quibusdam delectus, reprehenderit mollitia, quos nihil officiis
-        necessitatibus aliquam alias autem ab. Recusandae?Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Distinctio molestiae qui hic amet rem
-        voluptatibus accusamus praesentium maiores! Iste cum a culpa ipsum
-        architecto eos atque fuga ipsa? Velit aliquid blanditiis, nemo est aut
-        doloribus non neque eaque suscipit laudantium adipisci illum veritatis
-        earum ducimus dignissimos architecto provident veniam fugit, magni unde
-        repellat? Corrupti, suscipit facere sequi vero ipsa voluptate architecto
-        dignissimos impedit nesciunt libero maiores illo debitis! Quibusdam
-        similique maiores, quis eligendi ad corrupti impedit ipsa amet
-        doloremque dolore commodi culpa minus esse deleniti autem, nisi repellat
-        tempora consequatur deserunt inventore, omnis exercitationem officiis
-        quos debitis. Sed suscipit quasi iusto quibusdam ut laboriosam fugiat
-        asperiores quaerat libero nam perspiciatis unde a voluptatum quam natus
-        dignissimos reprehenderit voluptatibus in ratione dolores, eveniet
-        quidem. Error, dignissimos explicabo culpa sit facere debitis
-        consequuntur nobis quod odio fugit quos in, dolore nisi aliquid
-        aspernatur, officiis sequi deserunt doloribus minus rerum molestiae
-        voluptatem fugiat beatae? Quod aliquam, id vel doloribus ipsam unde
-        autem similique! A commodi sint, porro quisquam temporibus quaerat et
-        delectus, dolore neque possimus minima laudantium maxime, quia accusamus
-        esse sequi dolorem aspernatur quibusdam cum tempora eum. Explicabo saepe
-        exercitationem placeat tempore, voluptatum repudiandae fuga, culpa unde,
-        eius officiis blanditiis cumque iure.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
