5b9762d41847932eea1e05b61135b6fad4f0d744
Huihui <lihuihui99@outlook.com>
2025-09-23 11:47:51 +0800

    2.6.3 exercise 3: modifying an object in a list property

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Chilli"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
 
   return (
     <div>
-      {pizza.toppings.map((p) => (
-        <p key="p">{p}</p>
+      {cart.items.map((item) => (
+        <p>
+          {item.title}, {item.quantity}
+        </p>
       ))}
-      {/* <p>{pizza.toppings}</p> */}
       <button onClick={handleClick}>Click me</button>
     </div>
   );
